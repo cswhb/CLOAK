b17fea08d5e3979ff929ef24014658781d5eccc6
cswhb <846538430@qq.com>
2020-08-28 10:57:38 +0800

    Update twlmm_climber.py

--- twlmm_climber.py
+++ twlmm_climber.py
@@ -4,19 +4,19 @@ import numpy as np
 import math
 
 
-tracepath = 'trace.dat'
+tracepath = 'whb_trace.dat'
 ##############################
 ##############################
 pageshift = 12
 
 interinterval = 128
-swapthreshold = 32 ##
+swapthreshold = 32 #
+##########################################################
 mu = 0.3 
 sigma = mu*0.11 #
 
 class memorymodel:
     def __init__(self, areasize, attacktype, no, areashift, randomenable, randomshift):
-
         self.maxpagenums = areasize
         self.attacktype = attacktype
         print('maxpagenums' + str(self.maxpagenums))
@@ -25,16 +25,14 @@ class memorymodel:
         areanums = self.maxpagenums >> 10
         p = np.random.normal(loc = mu, scale = sigma, size = 2*areanums)
         p.sort()
-        
         x = [0 for i in range(self.maxpagenums)]
         for i in range(self.maxpagenums):
             x[i] = math.pow(p[areanums+(i>>10) - 1],-12)*90.345
         print("gen life distribution end")
         self.lifelist = [[0,x[0]] for y in range(len(x))]
         self.lifelist2 = [[0,x[0]] for y in range(len(x))]
-        #self.ideallifelist = [0 for y in range(len(x))]
         self.interswapcount = [0 for y in range(len(x))]
-        minlifetime = mu
+        minlifetime = 1000000000
         maxlifetime = 0
         for i in range(len(x)) :
             if(minlifetime > x[i]):
@@ -42,14 +40,14 @@ class memorymodel:
             if(maxlifetime < x[i]):
                 maxlifetime = x[i]
             self.lifelist[i][0] = i
-            self.lifelist[i][1] = x[i]#
+            self.lifelist[i][1] = x[i]
             self.lifelist2[i][0] = i
-            self.lifelist2[i][1] = x[i]#
+            self.lifelist2[i][1] = x[i]
         print("minlifetime =%d,maxlifetime =%d"%(int(minlifetime),int(maxlifetime)))
         x = []
         self.pairlist = [0 for m in range(self.maxpagenums)]
         print("sort pages begin")
-        self.sortedlist = sorted(self.lifelist2, key = lambda x:x[1])#
+        self.sortedlist = sorted(self.lifelist2, key = lambda x:x[1])
         print("sort pages end")
         print("pair pages begin")
         for i in range(len(self.sortedlist)):
@@ -60,16 +58,17 @@ class memorymodel:
         for i in range(len(self.intermaptable)):
             self.intermaptable[i] = i
             self.reversemaptable[i] = i
-        self.isswap =  [0 for m in range(int(self.maxpagenums/2))] 
-        self.swapvisitcount = [0 for m in range(int(self.maxpagenums))]#
+        self.isswap =  [0 for m in range(int(self.maxpagenums/2))]
+        self.swapvisitcount = [0 for m in range(int(self.maxpagenums))]
         self.swaptimes = 0
         self.interswaptimes = 0
     
         self.no = no
-        self.endlifepath = 'type' + str(self.attacktype)+'_twlmm_climber_60_endlife.dat'
+        self.endlifepath = 'type' + str(self.attacktype)+'_twlmm_endlife.dat'
+    
         self.totalcount = 0
         self.remaptimes = 0
-        self.totaltime = 0##
+        self.totaltime = 0
     def getpairaddr(self, interswapcount,clifelist,areasize,addr_temp, intermaptable, reversemaptable,sourceaddr):
         raddr = 0
         sourceaddr2 = 0
@@ -94,20 +93,13 @@ class memorymodel:
                 pairaddr0 = intermaptable[addr]
                 sourceaddr0 = 0
             while addr_next == intermaptable[addr]:
-                addr_next = random.randint(0,areasize-1)           
-                if addr_next >=self.maxpagenums / 2:
-                    pairaddr = self.maxpagenums - 1 - addr_next
-                    sourceaddr2 = 1
-                else:
-                    pairaddr = addr_next
-                    sourceaddr2 = 0
-                if sourceaddr0 ^ self.isswap[pairaddr0] != sourceaddr2 ^ self.isswap[pairaddr]:
-                    continue
+                addr_next = random.randint(0,areasize-1)
 
             if sourceaddr0 ^ self.isswap[pairaddr0] == 0:
                 clifelist[self.sortedlist[intermaptable[addr]][0]][1] = clifelist[self.sortedlist[intermaptable[addr]][0]][1] - 1 
             else:
                 clifelist[self.sortedlist[areasize - 1 - intermaptable[addr]][0]][1] = clifelist[self.sortedlist[areasize - 1 - intermaptable[addr]][0]][1] - 1    
+
             temp = intermaptable[addr]
             intermaptable[addr] = addr_next
             reverseaddr = reversemaptable[addr_next]
@@ -160,7 +152,7 @@ class memorymodel:
             isdoswap = 0
             swaptemp = 0
             if sourceaddr == 1:
-                swaptemp = self.swaparbiter(self.lifelist[nowaddr2][1],self.lifelist[addr][1])#####在执行过程中根据当前寿命调整
+                swaptemp = self.swaparbiter(self.lifelist[nowaddr2][1],self.lifelist[addr][1])#
             else:
                 swaptemp = self.swaparbiter(self.lifelist[addr][1],self.lifelist[nowaddr2][1]) 
             if swaptemp ^ self.isswap[pairaddr] == 1:
@@ -175,7 +167,7 @@ class memorymodel:
             else:
                 #self.noswaptimes[pairaddr] = self.noswaptimes[pairaddr] + 1 
                 if sourceaddr ^ self.isswap[pairaddr] == 0:
-                    self.lifelist[addr][1] = self.lifelist[addr][1] - 1##
+                    self.lifelist[addr][1] = self.lifelist[addr][1] - 1
                     if self.lifelist[addr][1] < 0:
                         return (-1,0)
                 else:
@@ -184,7 +176,7 @@ class memorymodel:
                         return (-1,0)
         else:
             if sourceaddr ^ self.isswap[pairaddr] == 0:
-                self.lifelist[addr][1] = self.lifelist[addr][1] - 1#
+                self.lifelist[addr][1] = self.lifelist[addr][1] - 1
                 if self.lifelist[addr][1] < 0:
                     return (-1,0)
             else:
@@ -192,30 +184,14 @@ class memorymodel:
                 if self.lifelist[self.sortedlist[self.maxpagenums - 1 - self.pairlist[addr]][0]][1] < 0:
                     return (-1,0)
         self.totalcount = self.totalcount + 1
-        if (self.totalcount - (2000000)) % (20000000) == 0:
-            lifenowlist = sorted(self.lifelist, key = lambda x:x[1])
-            maxwearrate = 0.0
-            wearrate = 0.0
-            maxlife = 0
-            maxlife2 = 0
-            maxi = 0
-            mixi2 = 0
-            for i in range(len(self.lifelist)):
-                wearrate = 1.0- (self.lifelist[i][1]/self.lifelist2[i][1])
-                if maxwearrate < wearrate:
-                    maxwearrate = wearrate
-                    maxlife = self.lifelist[i][1]
-                    maxlife2 = self.lifelist2[i][1]
-                    maxi = i
-            mixi2 = self.sortedlist[self.maxpagenums - 1 - self.pairlist[maxi]][0]
         return (0,0)
     def printstat(self):
-        print("write endstat start")
+        print("write endstat start")   #
         print('swaptime:%d'%(self.swaptimes))
         print('interswaptime:%d'%(self.interswaptimes))
         with open(self.endlifepath,'w') as f2:
             for i in range(len(self.lifelist)):
                 f2.write(U"%d,%d,%d\n"%(self.lifelist[i][0],int(self.lifelist[i][1]),int(self.lifelist2[i][1])))
         print("write endstat end")
-        print('totaltime:%d'%(self.totaltime))    
+        print('totaltime:%d'%(self.totaltime))     
         print("print end");
